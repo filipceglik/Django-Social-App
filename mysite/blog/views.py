18c858f70f665d7f52cf98114bc254085943c5b3
Filip Ceglik <fceglik@gmail.com>
2017-11-28 20:28:19 +0100

    typos fixed

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from blog.models import Post,Comment
 from blog.forms import PostForm,CommentForm
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
 
@@ -19,25 +19,25 @@ class PostListView(ListView):
 
 
 class PostDetailedView(DetailView):
-    model - Post
+    model = Post
 
-class CreatePostView(LoginRequredMixin, CreateView):
+class CreatePostView(LoginRequiredMixin, CreateView):
     login_url = '/login/'
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
     model = Post
 
-class PostUpdateView(LoginRequredMixin,UpdateView):
+class PostUpdateView(LoginRequiredMixin,UpdateView):
     login_url = '/login/'
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
     model = Post
 
-class PostDeleteView(LoginRequredMixin,DeleteView):
+class PostDeleteView(LoginRequiredMixin,DeleteView):
     model = Post
     success_url = reverse_lazy('post_list')
 
-class DraftListView(LoginRequredMixin,ListView):
+class DraftListView(LoginRequiredMixin,ListView):
     login_url = '/login/'
     redirect_field_name = 'blog/post_list.html'
     model = Post
